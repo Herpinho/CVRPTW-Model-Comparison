1fde0e2cb2b62107b7c216b12a28a25d89677793
Herpinho <ml.duarte.2605@gmail.com>
2026-05-03 20:53:20 +0100

    Minizinc performance boost
    Applied sorting system for the data before translating to minizinc data file.
    Decreased running time for clustered 25 customer data-set by 75% and obtained competitive results (191.8)

--- Project Files/MiniZinc/Minizinc.py
+++ Project Files/MiniZinc/Minizinc.py
@@ -6,26 +6,44 @@ def timer(done):
     start = time.time()
     while not done.wait(timeout=1):
         print(f"{int(time.time() - start)}s elapsed.", end="\r")
-def solve_cvrptw(mzn_file, dzn_file):
+def solve_cvrptw(mzn_file, dzn_file,size):
     if not mzn_path.exists() or not dzn_path.exists():
         print(f"Error: Could not find files.\nMZN: {mzn_path}\nDZN: {dzn_path}")
         return
     model = minizinc.Model(mzn_file)
     model.add_file(dzn_file)
+#   if int(size) < 50:
+#        model.add_string("""
+#        solve :: seq_search([
+#            int_search(array1d(successor), first_fail, indomain_min),
+ #           int_search(arrival, smallest, indomain_min) 
+#        ])
+#          :: restart_luby(250)
+#          minimize score;
+#        """)
+ #   else:
+  #      model.add_string("solve minimize score;") 
     solver = minizinc.Solver.lookup("chuffed")
     instance = minizinc.Instance(solver, model)
     done = threading.Event()
     threading.Thread(target=timer, args = (done, ), daemon=True).start()
 
-    result = instance.solve(timeout=timedelta(seconds = 300))
-    if result:
-        best_result = result[-1]
-        print(f"Best Score after 5 mins: {best_result.objective}")
+    result = instance.solve(timeout=timedelta(seconds = 1800))
     done.set()
     if result.solution is not None:
         print("\nSuccess!")
+        used_vehicles = 0
+        for v_route in result.solution.successor:
+
+            if v_route[0] != 1: 
+                used_vehicles += 1
         for v in range(len(result.solution.successor)):
+            raw_score = result.objective
+            actual_score = raw_score - (used_vehicles * 20000)
+            
+           
             successors = result.solution.successor[v]
+
             if successors[0] != 1:
                 route = []
                 curr = successors[0]
@@ -33,13 +51,15 @@ def solve_cvrptw(mzn_file, dzn_file):
                     route.append(curr)
                     curr = successors[curr - 1]
             
-            print(f"Vehicle {v+1}: Depot -> {' -> '.join(map(str, route))} -> Depot")
+                print(f"Vehicle {v+1}: Depot -> {' -> '.join(map(str, route))} -> Depot")
+        print(f"Final Score: {actual_score}\n")
     else:
         print("\nNo solution found.")
 
 if __name__ == "__main__":
-    mzn_file = input("Insert the model you wish to use (BigM/PH/PH): \n ->") + ".mzn"
-    dzn_file = input("Insert the Solomon file to use (c/r/rc): \n -> ") + "101.dzn"
+    mzn_file = (input("Insert the model you wish to use (BigM/PH/PH): \n ->"))
+    mzn_file = mzn_file + '.mzn' if mzn_file else 'bigm.mzn'
+    dzn_file = input("Insert the Solomon file to use : \n -> ") + ".dzn" 
     dzn_path = Path(__file__).parent.parent / "solomon" / "dzn" / dzn_file
     mzn_path = Path(__file__).parent / mzn_file
-    solve_cvrptw(mzn_path,dzn_path)
+    solve_cvrptw(mzn_path,dzn_path,size= "".join(filter(str.isdigit, dzn_file)))
--- Project Files/MiniZinc/TXT-to-DZN.py
+++ Project Files/MiniZinc/TXT-to-DZN.py
@@ -2,19 +2,29 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-
+import math
 load_dotenv()
 SOLOMON_DIRECTORY = Path(__file__).parent.parent / "solomon"
 OUTPUT_DIRECTORY = SOLOMON_DIRECTORY / "dzn"
 PRECISION_LEVEL = int(os.getenv("PRECISION_LEVEL"))
 
-def converter(solomon):
-    with open(SOLOMON_DIRECTORY / solomon,'r') as f:
+def converter(solomon,size,vehicount):
+    with open(SOLOMON_DIRECTORY / (solomon + "101.txt"),'r') as f:
         lines = f.readlines()
         veh_info = lines[4].split()
         data = [line.split() for line in lines[9:] if line.strip()]
     cust_no, x, y, demand, ready, due, service = [], [], [], [], [], [], []
-    for row in data:
+    depot_row = data[0]
+    xdepot = int(depot_row[1])
+    ydepot = int(depot_row[2])
+    customers = data[1:int(size) + 1]
+    #Priority ordering, might just be the most important thing for this god-forsaken model.
+    if "R" in solomon:
+        customers.sort(key=lambda row: ((int(row[5]) - int(row[4])) * PRECISION_LEVEL) - (math.hypot(int(row[1]) - xdepot, int(row[2]) - ydepot) * PRECISION_LEVEL))
+            
+    sorted = [depot_row] + customers
+
+    for row in sorted:
         cust_no.append(row[0])
         x.append(row[1])
         y.append(row[2])
@@ -22,9 +32,9 @@ def converter(solomon):
         ready.append(str(int(row[4])*PRECISION_LEVEL))
         due.append(str(int(row[5])*PRECISION_LEVEL))
         service.append(str(int(row[6])*PRECISION_LEVEL))
-    with open(OUTPUT_DIRECTORY / solomon.replace(".txt", ".dzn"), 'w') as dzn:
+    with open(OUTPUT_DIRECTORY / (solomon + str(size) + ".dzn"), 'w') as dzn:
         dzn.write(f"custcount = {len(cust_no) - 1};\n")
-        dzn.write(f"vehicount = {veh_info[0]};\n")
+        dzn.write(f"vehicount = {vehicount if vehicount else veh_info[0]};\n")
         dzn.write(f"capacity = {int(veh_info[1])*PRECISION_LEVEL};\n\n")
         dzn.write(f"x = [{', '.join(x)}];\n")
         dzn.write(f"y = [{', '.join(y)}];\n")
@@ -34,5 +44,9 @@ def converter(solomon):
         dzn.write(f"service = [{', '.join(service)}];\n")
 
 if __name__ == "__main__":
-    solomon = input('Insert file name (c/r/rc)\n ->') + "101.txt"
-    converter(solomon)
+    solomon = input('Insert file name (c/r/rc)\n ->')
+    size = input('Insert ammount of customers you wish to have: \n ->') 
+    size = size or 101
+    vehicount = input('Insert ammount of vehicles you wish to have: \n ->') 
+    vehicount = vehicount or 25
+    converter(solomon,size,vehicount)
